1d0bdcdb558eb2600bf747c612a8130445588a90
Michael Torres <mrmojorisen0@gmail.com>
2025-06-24 19:02:45 -0400

    Getting the data file implementation going.

---
+++ mod-4/final-assessments/course1/src/data.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+ * Copyright (C) 2025 by Michael Torres
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Michael Torres is not liable for any misuse of this material.
+ *
+ *****************************************************************************/
+/**
+ * @file data.c
+ * @brief Abstraction of string and integer operations
+ *
+ * This is the implementation file that provides an abstraction of using
+ * provided integers or strings and changing them into the other respective
+ * form.
+ *
+ * @author Michael Torres
+ * @date June 22, 2025
+ *
+ */
+#include "data.h"
+
+/*******************************************************************************
+ Function Definitions
+*******************************************************************************/
+uint8_t my_itoa(const int32_t data, uint8_t *const ptr, const uint32_t base) {
+}
