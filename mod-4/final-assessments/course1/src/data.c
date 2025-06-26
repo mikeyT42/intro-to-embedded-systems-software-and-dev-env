054cf7b9fea6db198826167124635ca5024894e2
Michael Torres <mrmojorisen0@gmail.com>
2025-06-25 20:39:06 -0400

    DAMN! That was pretty hard, not gonna lie.

--- mod-4/final-assessments/course1/src/data.c
+++ mod-4/final-assessments/course1/src/data.c
@@ -21,8 +21,51 @@
  */
 #include "data.h"
 
+char *recursive_itoa(char *buff_pos, const uint32_t quotient,
+                     const uint32_t base, uint8_t *const length);
+char itoc(const uint32_t remainder, const uint32_t base);
+
 /*******************************************************************************
  Function Definitions
 *******************************************************************************/
 uint8_t my_itoa(const int32_t data, uint8_t *const ptr, const uint32_t base) {
+  const uint8_t MAX_LEN = 34; // Includes the sign and \0.
+  char buff[MAX_LEN];
+
+  uint8_t length = 0;
+  if (data % 2 == 0) {
+    // blah
+  } else {
+    uint32_t made_pos = (~data) + 1;
+    char *start = buff;
+    *start = '-';
+    char *buff_pos = start + 1;
+    buff_pos = recursive_itoa(buff_pos, made_pos, base, &length);
+    *buff_pos = '\0';
+  }
+
+  return length;
+}
+
+// -----------------------------------------------------------------------------
+char *recursive_itoa(char *buff_pos, const uint32_t quotient,
+                     const uint32_t base, uint8_t *const length) {
+  if (quotient == 0)
+    return buff_pos;
+
+  const uint32_t new_quotient = quotient / base;
+  const uint32_t remainder = quotient % base;
+  char converted = itoc(remainder, base);
+  *buff_pos = converted;
+  *length += 1;
+  return recursive_itoa(++buff_pos, new_quotient, base, length);
+}
+
+// -----------------------------------------------------------------------------
+char itoc(const uint32_t remainder, const uint32_t base) {
+  uint8_t ascii_inc = 48;
+  if (base > 10 && remainder > 9)
+    ascii_inc = 55;
+
+  return (char)remainder + ascii_inc;
 }
